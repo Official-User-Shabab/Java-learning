e4f410e07ab2cd073d7229a0bec5fa97d45ea796
Official-User-Shabab <officialshabab24@gmail.com>
2024-07-09 14:25:45 +0100

    Create Structure of a program.java

---
+++ Structure of a program.java
@@ -0,0 +1,26 @@
+/* Ensure to have downloaded a suitable editor for java language before starting to code. 
+Common editors are:
+- IntelliJ
+- Visual Studio Code
+- Eclipse
+- NetBeans
+- Notepad++
+You will also need to download a jdk (java development kit)
+The structure of a java program in general is shown below: */
+
+// There would be packages stated here in first line
+
+// Packages have classes, which have functions
+// "Main", here, is a class. public (or private) is the type of class
+// "main", here, is a function, in the class (can be called a method).
+// The RETURN type of function below is "void". The type of this function is static
+
+
+public class Main {
+  
+  public static void main(String[] args) {
+    // Any code goes here
+
+    System.out.println("Hi"); // Statements are terminated with ";"
+  }
+}
